c6ecfc0a5f6a4f12a70f6d8a723b61da43d28be0
Jordan Cook <jordan.cook@pioneer.com>
2022-02-26 14:49:08 -0600

    Remove logo anmiation

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -1,48 +1,14 @@
 <template>
   <div class="logo-wrapper">
-    <div
-      :class="{
-        'logo-inner': true,
-        blink: blinking
-      }"
-    >
-      <img
-        v-for="x in frames"
-        :key="x"
-        :src="`/logo-frames/logo-f${x - 1}.svg`"
-      />
-      <img
-        v-for="x in frames"
-        :key="`${x}-r`"
-        :src="`/logo-frames/logo-f${frames - x}.svg`"
-      />
+    <div :class="{ 'logo-inner': true }">
+      <img src="/logo.svg" />
     </div>
   </div>
 </template>
 <script>
-export default {
-  name: 'SiteLogo',
-  data() {
-    return {
-      frames: 5,
-      currentFrame: 1,
-      blinking: true
-    }
-  },
-  mounted() {
-    setInterval(() => {
-      const rand = Math.ceil(Math.random() * 10)
-      this.blinking = rand % 2
-    }, 1000)
-  }
-}
+export default { name: 'SiteLogo' }
 </script>
 <style lang="scss">
-@keyframes logoBlink {
-  100% {
-    transform: translateY(-360px);
-  }
-}
 .logo-wrapper {
   position: relative;
   height: 50px;
@@ -56,9 +22,6 @@ export default {
       height: 40px;
       margin: 5px;
       max-height: initial;
-    }
-    &.blink {
-      animation: logoBlink 0.2s steps(8, end) alternate;
     }
   }
 }
